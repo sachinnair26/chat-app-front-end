973859139b69802c50945d4fe6fc2931cd27e11c
sachinnair26 <sachin.nair32@gmail.com>
2019-05-20 13:22:07 +0530

    firstr commit

---
+++ src/Components/Login/index.js
@@ -0,0 +1,13 @@
+import React,{useState} from 'react';
+import './index.css';
+function Login () {
+    return (
+        <div className='login-wrapper'>
+            <div className='credential-wrapper'>
+                <h1>My Crappy ChatApp</h1>
+                
+            </div>
+        </div>
+    )
+}
+export default Login;
