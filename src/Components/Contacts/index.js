805890a6e896b152acc9ed290a5a5e3809c3606e
sachinnair26 <sachin.nair32@gmail.com>
2019-09-28 22:32:14 +0530

    search done

--- src/Components/Contacts/index.js
+++ src/Components/Contacts/index.js
@@ -3,14 +3,24 @@ import {connect} from 'react-redux';
 import './index.css';
 import GetContactsAction from '../Action/GetContactsAction';
 import SelectContactAction from '../Action/SelectContactAction';
-
+import {socket} from '../Home';
 function Contacts({contacts,GetContactsAction,SelectContactAction}){
- useEffect(()=>{
-     GetContactsAction()
- },[])
- 
+    const [search_result,set_search_result] = useState([])
+    useEffect(()=>{
+        GetContactsAction()
+        socket.on('search-result',function(val){
+            set_search_result(val)
+            console.log(val);
+            
+        })
+    },[])
+   
     return(
         <div>
+        <div className='search'>
+            <input onChange={(e)=>{e.target.value.length >0 ? socket.emit('contact-search',e.target.value):set_search_result([])}}/> 
+            <div className='search-param'>{search_result.length >0 ? search_result.map(res =>(<div>{res}</div>)):null}</div>
+        </div>
             {Object.keys(contacts).map(contact =><div className='single-contact' key={contact} onClick={()=>SelectContactAction(contact)}>
             {contact}
             </div>)}
