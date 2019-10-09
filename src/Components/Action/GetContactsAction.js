bb54c2dd08214acef0843c9f240095368f442d89
sachinnair26 <sachin.nair32@gmail.com>
2019-10-09 21:25:43 +0530

    before modifing contact fetch

--- src/Components/Action/GetContactsAction.js
+++ src/Components/Action/GetContactsAction.js
@@ -8,11 +8,10 @@ export default function GetContactsAction(){
                 return point.json()
                 
             }).then(val =>{
-                var tim ={}
-                tim[val.name] = val.mesg_ref
+               
                 dispatch({
                     type:GET_CONTACTS,
-                    contacts:tim
+                    contacts:val
                 })
             })
         }
--- src/Components/Contacts/index.js
+++ src/Components/Contacts/index.js
@@ -5,12 +5,13 @@ import GetContactsAction from '../Action/GetContactsAction';
 import SelectContactAction from '../Action/SelectContactAction';
 import {socket} from '../Home';
 function Contacts({contacts,GetContactsAction,SelectContactAction}){
+    console.log(contacts);
+    
     const [search_result,set_search_result] = useState([])
     useEffect(()=>{
         GetContactsAction()
         socket.on('search-result',function(val){
             set_search_result(val)
-            console.log(val);
             
         })
     },[])
@@ -21,8 +22,8 @@ function Contacts({contacts,GetContactsAction,SelectContactAction}){
             <input onChange={(e)=>{e.target.value.length >0 ? socket.emit('contact-search',e.target.value):set_search_result([])}}/> 
             <div className='search-param'>{search_result.length >0 ? search_result.map(res =>(<div>{res}</div>)):null}</div>
         </div>
-            {Object.keys(contacts).map(contact =><div className='single-contact' key={contact} onClick={()=>SelectContactAction(contact)}>
-            {contact}
+            {Object.keys(contacts).map(contact =><div className='single-contact' key={contacts[contact].name} onClick={()=>SelectContactAction(contacts[contact].name)}>
+            {contacts[contact].name}
             </div>)}
             
         </div>
